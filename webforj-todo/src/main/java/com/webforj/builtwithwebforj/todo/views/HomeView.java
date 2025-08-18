b53c8cefd5e0b1c7e2af571945440bc86fd2a5fd
MatthewHawkins <matthew.hawkins1115@gmail.com>
2025-08-18 10:08:39 -0400

    feat: init commit

---
+++ webforj-todo/src/main/java/com/webforj/builtwithwebforj/todo/views/HomeView.java
@@ -0,0 +1,21 @@
+package com.webforj.builtwithwebforj.todo.views;
+
+import com.webforj.builtwithwebforj.todo.components.Explore;
+
+import com.webforj.component.Composite;
+import com.webforj.component.layout.flexlayout.FlexAlignment;
+import com.webforj.component.layout.flexlayout.FlexLayout;
+import com.webforj.router.annotation.FrameTitle;
+import com.webforj.router.annotation.Route;
+
+@Route(value = "/", outlet = MainLayout.class)
+@FrameTitle("Blank")
+public class HomeView extends Composite<FlexLayout> {
+  private FlexLayout self = getBoundComponent();
+
+  public HomeView() {
+    self.setHeight("100%");
+    self.setAlignment(FlexAlignment.CENTER);
+    self.add(new Explore());
+  }
+}
